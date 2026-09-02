385b15f8ab14e554073e7137ffbc6170a5415b20
Thomas Boch <tboch@churchtools.de>
2026-09-02 22:49:52 +0200

    Shrink release binary symbols

--- build-app.sh
+++ build-app.sh
@@ -35,6 +35,7 @@ export SWIFT_MODULE_CACHE_PATH="$MODULE_CACHE"
 swiftc -parse-as-library -swift-version 5 -sdk "$SDK_PATH" -target arm64-apple-macos13.0 Sources/*.swift \
   -o "$APP/Contents/MacOS/$EXECUTABLE" \
   -framework SwiftUI -framework AppKit -framework ServiceManagement -framework EventKit -framework CoreAudio
+strip -x "$APP/Contents/MacOS/$EXECUTABLE"
 
 swiftc -sdk "$SDK_PATH" -target arm64-apple-macos13.0 make-icon.swift -o .build/make-icon
 .build/make-icon "$ICONSET"
